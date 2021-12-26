bce51ee8375327f934db811c221672d9886351b4
Cory Sanin <corysanin@outlook.com>
2021-12-26 01:25:07 -0600

    Load arbitrary URL

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,5 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
     var modulebtns = document.querySelectorAll('#modules button');
+    var urltxt = document.getElementById('urltxt');
+    var submitbtn = document.getElementById('submitbtn');
+
+    function submitUrl() {
+        fetch('api/load', {
+            method: 'POST',
+            cache: 'no-cache',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                type: 'url',
+                body: urltxt.value
+            })
+        });
+        urltxt.value = '';
+    }
+
+    submitbtn.addEventListener('click', submitUrl);
+    urltxt.addEventListener('keyup', function (e) {
+        if (e.key === 'Enter') {
+            submitUrl();
+        }
+    });
+
     modulebtns.forEach(function (btn) {
         btn.addEventListener('click', function (e) {
             let mname = btn.querySelector('.module-name').innerText;

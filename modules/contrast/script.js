68576c1acc4941907d3adca2b45921e31f0b736a
Cory Sanin <corysanin@outlook.com>
2021-12-26 17:40:53 -0600

    Second hand works forever ♾

--- modules/contrast/script.js
+++ modules/contrast/script.js
@@ -1,24 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
     var minuteh = document.getElementById('minute');
     var hourh = document.getElementById('hour');
-    var secondh = document.getElementById('second');
-    var starttime = new Date();
-    starttime.setSeconds(0);
-
-    function getParams() {
-        const urlParams = new URLSearchParams(window.location.search);
-        const color = urlParams.get('color');
-        document.body.style.backgroundColor = color;
-    }
+    var s1 = document.querySelector('.second.s1');
+    var s2 = document.querySelector('.second.s2');
+    var secondh = s1;
+    var prevseconds = (new Date()).getSeconds();
 
     function updateHands() {
         var d = new Date();
-        var sec = Math.floor((d.getTime() - starttime.getTime()) / 1000);
+        var sec = d.getSeconds();
+        if(prevseconds > 0 && sec === 0){
+            sec = 60;
+            if(prevseconds < sec){
+                swapHands();
+            }
+        }
+        if(sec === 30 && sec > prevseconds){
+            resetHand();
+        }
+        prevseconds = sec;
         var min = d.getMinutes() + ((d.getTime() / 1000 % 60) / 60);
         var hour = d.getHours() + (min / 60);
         secondh.style.transform = `rotate(${sec * 6}deg)`;
         minuteh.style.transform = `rotate(${min * 6}deg)`;
         hourh.style.transform = `rotate(${hour * 30}deg)`;
+    }
+
+    function swapHands() {
+        setTimeout(function () {
+            prevseconds = 0;
+            secondh.style.visibility = 'hidden';
+            secondh = (secondh === s1) ? s2 : s1;
+            secondh.style.visibility = 'visible';
+        }, 500);
+    }
+
+    function resetHand() {
+        (secondh === s1 ? s2 : s1).style.transform = 'rotate(0deg)';
     }
 
     setInterval(updateHands, 50);
